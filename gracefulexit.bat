191ca674e2450b60f2d90277a49d9bad877bcd17
Keith Gorlen <gorlen@comcast.net>
2020-11-17 19:51:37 -0800

    gracefulexit.bat Version 2.1: - Kill multiple instances of same task - Replace faulting nircmd trayballon with Powershell command
    Sleep.lnk, Switch.lnk:
    - Add putty.exe
    
    Update *.ffs_batch for Photoshop/Bridge 2021

--- gracefulexit.bat
+++ gracefulexit.bat
@@ -1,16 +1,28 @@
 @echo off
+rem
 rem Kill tasks that lock shared files, then sleep, switch user, or lock session
+rem
 rem Usage:
 rem		gracefulexit {sleep|switch|lock} task1 task2 ...
 rem
 rem Installation:
+rem		Customize location of FreeFileSync .ffs_batch file, if needed -- see below
 rem		Copy gracefulexit.bat to C:\Program Files\gracefulexit
 rem		Copy sleep.lnk, switch.lnk, lock.lnk, logoff.lnk to:
 rem			C:\ProgramData\Microsoft\Windows\Start Menu\Programs\gracefulexit
+rem		Pin GracefileExit -> sleep.lnk, switch.lnk, lock.lnk, logoff.lnk to Start Menu
 rem		Install nircmd/nircmdc (http://www.nirsoft.net/utils/nircmd.html):
 rem			Copy nircmd.exe, nircmdc.exe to C:\Windows\System32
 rem		Control Panel->Hardware and Sound->Power Options->System Settings
 rem			->Choose what the power button does: Disable Sleep and Lock menu items
+rem		
+rem Location of FreeFileSync .ffs_batch file:
+set syncfile=%APPDATA%\FreeFileSync\Config\full.ffs_batch
+
+rem	Version 2.1:
+rem
+rem 	Kill multiple instances of same task
+rem		Replace faulting nircmd trayballon with Powershell command
 
 if [%1]==[] (
 	echo Usage: gracefulexit {sleep^|switch^|lock^|logoff} task1 task2 ...
@@ -18,25 +30,18 @@ if [%1]==[] (
 )
 
 setlocal EnableExtensions
+setlocal EnableDelayedExpansion
 
 if /i %1==sleep (
-	set icon=shell32.dll,027
 	set action=nircmd standby
 ) else if /i %1==switch (
-	set icon=shell32.dll,238
 	set action=nircmd lockws
 ) else if /i %1==lock (
-	set icon=shell32.dll,047
-rem Download and install tsdiscon.exe in %SystemRoot%\System32, if necessary.
-rem	set action=%SystemRoot%\System32\tsdiscon.exe
-rem Using LockWorkStation, since same as (missing) tsdiscon on Win10
-rem Does NOT lock! -- same as tsdiscon.exe
-	set action=nircmd lockws
+rem LockWorkStation same as Switch User on Win10
+	set action=rundll32.exe user32.dll,LockWorkStation
 ) else if /i %1==logoff (
-	set icon=shell32.dll,112
 	set action=shutdown -l
 ) else (
-	set icon=shell32.dll,221
 	set action=echo Unrecognized action: %1
 )
 
@@ -45,22 +50,30 @@ if [%2]==[] goto SYNCFILES
 rem Some tasks (e.g. Quicken) will not close when minimized
 	nircmd win normal process %2
 	for /f "tokens=1,11 delims=: " %%G in ('taskkill /fi "USERNAME eq %USERNAME%" /im %2') do (
-		set result=%%G
-		set pid=%%H
+		if %%G==SUCCESS (
+			set pid=%%H
+			set pid=!pid:.=!
+			call :balloontip "GracefulExit %1" "Closing %2 PID !pid! ..." 3000
+
+			nircmd waitprocess /!pid!
 		)
-	set pid=%pid:.=%
-	if %result%==SUCCESS (
-		nircmd trayballoon "GracefulExit %2" "Closing PID %pid%..." "%icon%" 3000
-		nircmd waitprocess /%pid%
-		)
+	)
 	shift /2
 	goto KILLTASKS
 
 :SYNCFILES
-set syncfile=%APPDATA%\FreeFileSync\Config\full.ffs_batch
 if exist %syncfile% (
-	nircmd trayballoon "GracefulExit" "Synchronizing files..." "%icon%" 3000
+	call :balloontip "GracefulExit %1" "Synchronizing files ..." 3000
 	start "Synchronizing Files" /W "%ProgramFiles%\FreeFileSync\FreeFileSync.exe" "%syncfile%"
 )
 
 %action%
+exit /b
+
+:balloontip
+rem	Usage: call :balloontip Title TipText Delay
+rem	Reference:
+rem 	https://stackoverflow.com/questions/50927132/show-balloon-notifications-from-batch-file
+
+powershell -Command "[void] [System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms'); $objNotifyIcon=New-Object System.Windows.Forms.NotifyIcon; $objNotifyIcon.BalloonTipText='%~2'; $objNotifyIcon.Icon=[system.drawing.systemicons]::Information; $objNotifyIcon.BalloonTipTitle='%~1'; $objNotifyIcon.BalloonTipIcon='None'; $objNotifyIcon.Visible=$True; $objNotifyIcon.ShowBalloonTip(%~3);"
+exit /b
