cee9ebb28c0e915507d5e5b3d08e1dcc9cb645b2
Keith Gorlen <gorlen@comcast.net>
2021-04-15 14:41:55 -0700

    - Replace catmap.bat with permanent directory symlink and junction. - gracefulexit.bat: Retry taskkill if Wait-Process timeout; echo commands for debugging. - Update documentation.

--- gracefulexit.bat
+++ gracefulexit.bat
@@ -1,33 +1,38 @@
-@echo off
-rem
-rem Kill tasks that lock shared files, then sleep, switch user, or log off
-rem
-rem Usage:
-rem		gracefulexit {sleep|switch|lock} task1 task2 ...
-rem
-rem Installation:
-rem		Customize location of FreeFileSync .ffs_batch file, if needed -- see below
-rem		Copy gracefulexit.bat to C:\Program Files\gracefulexit
-rem		Copy sleep.lnk, switch.lnk, lock.lnk, logoff.lnk to:
-rem			C:\ProgramData\Microsoft\Windows\Start Menu\Programs\gracefulexit
-rem		Pin GracefileExit -> sleep.lnk, switch.lnk, lock.lnk, logoff.lnk to Start Menu
-rem		Control Panel->Hardware and Sound->Power Options->System Settings
-rem			->Choose what the power button does: Disable Sleep and Lock menu items
-rem		
-rem Location of FreeFileSync .ffs_batch file:
+@rem
+@rem Kill tasks that lock shared files, then sleep, switch user, or log off
+@rem
+@rem Usage:
+@rem		gracefulexit {sleep|switch|lock} task1 task2 ...
+@rem
+@rem Installation:
+@rem		Customize location of FreeFileSync .ffs_batch file, if needed -- see below
+@rem		Copy gracefulexit.bat to C:\Program Files\gracefulexit
+@rem		Copy sleep.lnk, switch.lnk, lock.lnk, logoff.lnk to:
+@rem			C:\ProgramData\Microsoft\Windows\Start Menu\Programs\gracefulexit
+@rem		Pin GracefileExit -> sleep.lnk, switch.lnk, lock.lnk, logoff.lnk to Start Menu
+@rem		Control Panel->Hardware and Sound->Power Options->System Settings
+@rem			->Choose what the power button does: Disable Sleep and Lock menu items
+@rem	
+@rem Location of FreeFileSync .ffs_batch file:
 set syncfile=%APPDATA%\FreeFileSync\Config\full.ffs_batch
 
-rem	Version 2.2:
-rem
-rem 	Replace nircmd with Powershell
-rem
-rem	Version 2.1:
-rem
-rem 	Kill multiple instances of same task
-rem		Replace faulting nircmd trayballon with Powershell command
+@rem Version 2.3:
+@rem
+@rem	Retry taskkill if Wait-Process timeout
+@rem 	Echo commands for debugging
+@rem	
+@rem	Version 2.2:
+@rem
+@rem 	Replace nircmd with Powershell
+@rem
+@rem	Version 2.1:
+@rem
+@rem 	Kill multiple instances of same task
+@rem		Replace faulting nircmd trayballon with Powershell command
+@rem
 
 if [%1]==[] (
-	echo Usage: gracefulexit {sleep^|switch^|lock^|logoff} task1 task2 ...
+	echo Usage: gracefulexit {sleep^|switch^|lock^|logoff^|pause} task1 task2 ...
 	exit /b 1
 )
 
@@ -39,17 +44,19 @@ if /i %1==sleep (
 ) else if /i %1==switch (
 	set action=rundll32.exe user32.dll,LockWorkStation
 ) else if /i %1==lock (
-rem LockWorkStation same as Switch User on Win10
+@rem LockWorkStation same as Switch User on Win10
 	set action=rundll32.exe user32.dll,LockWorkStation
 ) else if /i %1==logoff (
 	set action=shutdown -l
+) else if /i %1==pause (
+	set action=pause
 ) else (
 	set action=echo Unrecognized action: %1
 )
 
 :KILLTASKS
 if [%2]==[] goto SYNCFILES
-rem Some tasks (e.g. Quicken) will not close when minimized
+@rem Some tasks (e.g. Quicken) will not close when minimized
 	set pname=%2
 	call :restore %pname:.exe=%
 	for /f "tokens=1,11 delims=: " %%G in ('taskkill /fi "USERNAME eq %USERNAME%" /im %pname%') do (
@@ -57,7 +64,11 @@ rem Some tasks (e.g. Quicken) will not close when minimized
 			set pid=%%H
 			set pid=!pid:.=!
 			call :balloontip "GracefulExit %1" "Closing %pname% PID !pid! ..." 3000
-			powershell -Command "Wait-Process -Id !pid!"
+			powershell -Command "Wait-Process -Id !pid! -Timeout 10" | find /i "time-out"
+			if %ErrorLevel% equ 0 (
+				echo %pname% PID !pid! did not respond to taskkill, retrying ...
+				goto KILLTASKS
+			)
 		)
 	)
 	shift /2
@@ -73,26 +84,26 @@ if exist %syncfile% (
 exit /b
 
 :restore
-rem Usage: call :restore Name
-rem Reference:
-rem		https://community.idera.com/database-tools/powershell/powertips/b/tips/posts/show-or-hide-windows
-rem		
+@rem Usage: call :restore Name
+@rem Reference:
+@rem		https://community.idera.com/database-tools/powershell/powertips/b/tips/posts/show-or-hide-windows
+@rem		
 
 powershell -Command "$ErrorActionPreference = 'SilentlyContinue'; $code = '[DllImport(\"user32.dll\")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);'; $type = Add-Type -MemberDefinition $code -Name myAPI -PassThru; $process = Get-Process -Name '%~1'; $hwnd = $process.MainWindowHandle; $type::ShowWindowAsync($hwnd, 9)"
 exit /b
 
 :sleep
-rem Usage: call :sleep
-rem Reference:
-rem		https://superuser.com/questions/39584/what-is-the-command-to-use-to-put-your-computer-to-sleep-not-hibernate
+@rem Usage: call :sleep
+@rem Reference:
+@rem		https://superuser.com/questions/39584/what-is-the-command-to-use-to-put-your-computer-to-sleep-not-hibernate
 
 powershell -Command "Add-Type -Assembly System.Windows.Forms; [System.Windows.Forms.Application]::SetSuspendState('Suspend', $false, $true)"
 exit /b
 
 :balloontip
-rem	Usage: call :balloontip Title TipText Delay
-rem	Reference:
-rem 	https://stackoverflow.com/questions/50927132/show-balloon-notifications-from-batch-file
+@rem	Usage: call :balloontip Title TipText Delay
+@rem	Reference:
+@rem 	https://stackoverflow.com/questions/50927132/show-balloon-notifications-from-batch-file
 
 powershell -Command "[void] [System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms'); $objNotifyIcon=New-Object System.Windows.Forms.NotifyIcon; $objNotifyIcon.BalloonTipText='%~2'; $objNotifyIcon.Icon=[system.drawing.systemicons]::Information; $objNotifyIcon.BalloonTipTitle='%~1'; $objNotifyIcon.BalloonTipIcon='None'; $objNotifyIcon.Visible=$True; $objNotifyIcon.ShowBalloonTip(%~3);"
 exit /b
